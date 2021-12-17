e84b08c4fe791361529d8530b989a7695891af19
OphiliaFradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2021-12-17 06:08:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+//global variables
+var box;
+
+function setup(){
+  createCanvas(600, 400);
+
+  box = createSprite(280, 170, 30, 30);
+}
+
+function draw(){
+  background("black");
+
+  drawSprites();
+
+}
